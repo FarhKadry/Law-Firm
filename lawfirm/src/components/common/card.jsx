3c94151e92a42e2c85b6d0476d8de5c5a97708d3
Farh Kadry <23-101289@students.eui.edu.eg>
2026-02-20 16:13:46 +0200

    cards styled

--- lawfirm/src/components/common/card.jsx
+++ lawfirm/src/components/common/card.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './card.css';
 
-const Card = () => {
+const Card = (props) => {
     return ( <>
     <div className="cardFlex">
-        
+        <div className="cardTitle">
+            <h4>{props.title}</h4>
+            <img src={props.img} alt={props.title} />
+        </div>
+            <p>{props.desc}</p>
     </div>
     
     </> );
--- lawfirm/src/pages/home.jsx
+++ lawfirm/src/pages/home.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './home.css';
 
 import heroimg from '../assets/heroimg.png';
+import cardicon1 from '../assets/cardicon01.svg';
+import cardicon2 from '../assets/cardicon02.svg';
+import cardicon3 from '../assets/cardicon03.svg';
+import cardicon4 from '../assets/cardicon04.svg';
 
 import Navbar from '../components/layout/nav';
 import Button from '../components/common/button';
+import Card from '../components/common/card';
 
 const Home = () => {
     return ( <>
@@ -29,10 +34,12 @@ const Home = () => {
                 </div>
             </div>
         </section>
-
         <section>
             <div className="secContainer cardsContainer"> 
-                
+                <Card title="Family Law" desc="Navigating the complexities of family law with compassion and expertise. Our dedicated team provides comprehensive legal support for divorce, child custody, adoption, and more, ensuring your family's best interests are protected." img={cardicon1} />
+                <Card title="Criminal Law" desc="Defending your rights with unwavering commitment. Our experienced criminal defense team provides strategic representation for a wide range of cases, ensuring you receive a fair trial and the best possible outcome." img={cardicon2} />
+                <Card title="Corporate Law" desc="Guiding your business to success with expert legal counsel. Our corporate law services cover everything from formation and compliance to mergers and acquisitions, helping your company thrive in a complex legal landscape." img={cardicon3} />  
+                <Card title="Real Estate Law" desc="Navigating the complexities of real estate law with expertise and precision. Our team provides comprehensive legal support for property transactions, disputes, and regulatory compliance, ensuring your real estate interests are protected." img={cardicon4} />  
             </div>
         </section>
     </main>
