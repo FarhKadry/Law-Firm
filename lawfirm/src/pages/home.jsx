2075129dc3418655d8e7e0ffa4ae96c3c34362d0
Farh Kadry <23-101289@students.eui.edu.eg>
2026-02-20 12:19:38 +0200

    hero styled responsively

--- lawfirm/src/pages/home.jsx
+++ lawfirm/src/pages/home.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './home.css';
+
+import heroimg from '../assets/heroimg.png';
+
 import Navbar from '../components/layout/nav';
 
 const Home = () => {
@@ -13,6 +16,7 @@ const Home = () => {
                 <div className="hero">
                     <h1>Fighting with justice
 — for justice</h1>
+<img className='heroimg' src={heroimg} alt="hero" />
                 </div>
             </div>
         </section>
