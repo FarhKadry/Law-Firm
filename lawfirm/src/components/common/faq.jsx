cb7d4f8b5571a15e1a29b37b5e498536a2df6ce1
Farh Kadry <23-101289@students.eui.edu.eg>
2026-02-21 14:14:51 +0200

    question card component created

---
+++ lawfirm/src/components/common/faq.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Question = () => {
+    return ( <>
+    <div className="questFlex">
+        <div className="qsNo">
+            I
+        </div>
+        
+    </div>
+    </> );
+}
+ 
+export default Question;
