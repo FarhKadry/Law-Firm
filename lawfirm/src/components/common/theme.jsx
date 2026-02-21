0fbdce0c4a8c5a087f99807772e12802ae01c17e
Farh Kadry <23-101289@students.eui.edu.eg>
2026-02-21 16:47:18 +0200

    some edits

--- lawfirm/src/components/common/theme.jsx
+++ lawfirm/src/components/common/theme.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import './../../root.css'
+import dark from '../../assets/moon.svg'
+import light from '../../assets/sun.svg'
+
 
 function Theme() {
-const setTheme = (theme) => {
-document.documentElement.setAttribute("data-theme", theme);
-};
-return ( <>
-    <button onclick={() => setTheme ("light")}>Light Mode</button>
-    <button onClick={() => setTheme("dark")}>Dark Mode</button>
-</> );
+  const setTheme = (theme) => {
+    document.documentElement.setAttribute("data-theme", theme);
+  };
+
+  return (
+    <>
+      <button className="themeBtn" onClick={() => setTheme("light")}>
+        <img src={light} alt="" />
+      </button>
+      <button className="themeBtn" onClick={() => setTheme("dark")}>
+        <img src={dark} alt="" />
+      </button>
+    </>
+  );
 }
  
 export default Theme;
